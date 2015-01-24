88fe9cdc68f37d9e9e3f3940059a77b7470dae2e
ZKShao <zhikang.shao@gmail.com>
2015-01-24 11:51:25 +0100

    Revert "stuff"
    This reverts commit 2ee942123fae28e1617df6554076a6a3ebe2b62c.

--- GGJ2015/Source/GGJ2015/RoomDestroyer.cpp
+++ GGJ2015/Source/GGJ2015/RoomDestroyer.cpp
@@ -1,7 +1,6 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "GGJ2015.h"
-#include "Engine/StaticMeshActor.h"
 #include "RoomDestroyer.h"
 #include "EngineUtils.h"
 
