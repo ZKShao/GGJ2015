76809ded48cf5715644158f7d3e15e85a3bf8858
tkdejong <tim@raatdejong.nl>
2015-01-24 11:20:06 +0100

    Mogeljikheid om stukken level te laten vallen

---
+++ GGJ2015/Source/GGJ2015/RoomDestroyer.h
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/Actor.h"
+#include "RoomDestroyer.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GGJ2015_API ARoomDestroyer : public AActor
+{
+	GENERATED_BODY()
+	
+protected:
+	UFUNCTION(BlueprintCallable, Category = GGJ2015)
+  void DestroyRoomWithTag(FName targetTag);
+	
+  UFUNCTION(BlueprintImplementableEvent, Category = GGJ2015)
+  void DestroyObject(AStaticMeshActor* target);
+};
